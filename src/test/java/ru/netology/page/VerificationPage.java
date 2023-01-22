ffe41a54080e64af2cc48e29d57a7a494b88944f
Elena <blonde_90@gmail.com>
2023-01-22 15:43:04 +0300

    add

---
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -0,0 +1,32 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private SelenideElement errorNotification = $("[data-test-id=error-notification]");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(String verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+        return new DashboardPage();
+    }
+
+    public void invalidVerify(String errorText) {
+        codeField.setValue(DataHelper.generateRandomVerificationCode().getCode());
+        verifyButton.click();
+        errorNotification.shouldHave(text(errorText), Duration.ofSeconds(15)).shouldBe(visible);
+    }
+}
